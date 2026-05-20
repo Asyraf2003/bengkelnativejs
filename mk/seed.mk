2da282d171642fe79d0f62e54c5d3e6aa44b0e56
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-05-20 09:44:06 +0800

    commit 2197

--- mk/seed.mk
+++ mk/seed.mk
@@ -1,31 +1,31 @@
 .PHONY: seed-user
 seed-user:
-	php artisan db:seed --class='Database\\Seeders\\CreateOnly\\CreateUserSeeder'
+	php artisan db:seed --class='Database\Seeders\CreateOnly\CreateUserSeeder'
 
 .PHONY: user
 user: seed-user
 
 .PHONY: seed-create-basic
 seed-create-basic:
-	php artisan db:seed --class='Database\\Seeders\\CreateOnly\\CreateMasterBasicSeeder'
+	php artisan db:seed --class='Database\Seeders\CreateOnly\CreateMasterBasicSeeder'
 
 .PHONY: product-1
 product-1: seed-create-basic
 
 .PHONY: seed-create-week
 seed-create-week:
-	php artisan db:seed --class='Database\\Seeders\\CreateOnly\\CreateMasterDenseWeekSeeder'
+	php artisan db:seed --class='Database\Seeders\CreateOnly\CreateMasterDenseWeekSeeder'
 
 .PHONY: product-2
 product-2: seed-create-week
 
 .PHONY: seed-create-year
 seed-create-year:
-	php artisan db:seed --class='Database\\Seeders\\CreateOnly\\CreateMasterDenseYearSeeder'
+	php artisan db:seed --class='Database\Seeders\CreateOnly\CreateMasterDenseYearSeeder'
 
 .PHONY: product-year
 product-year: seed-create-year
 
 .PHONY: seed-create-default
 seed-create-default:
-	php artisan db:seed --class='Database\\Seeders\\DatabaseSeeder'
+	php artisan db:seed --class='Database\Seeders\DatabaseSeeder'
